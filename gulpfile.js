96a422ed423e14dc179bbe5f6598a4991b61d958
moritomomasa <s1870276@ems.u-toyama.ac.jp>
2021-09-16 14:47:59 +0900

    add_node

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+let gulp         = require('gulp');
+let fileinclude  = require('gulp-file-include');
+let connect      = require('gulp-connect');
+ 
+gulp.task('default', function() {
+  // place code for your default task here
+});
+ 
+gulp.task('connect', function() {
+  connect.server({
+    root: './',
+    livereload: true
+  });
+});
+ 
+gulp.task('html', function () {
+  gulp.src('./*.html')
+    .pipe(connect.reload());
+});
+ 
+gulp.task('watch', function () {
+  gulp.watch(['./*.html'], ['html']);
+});
+ 
+gulp.task('default', ['connect', 'watch']);
+ 
+gulp.task('include', function() {
+	gulp.src(['ja/download.html'])
+	  .pipe(fileinclude({
+		prefix: '@@',
+		basepath: '@file'
+	  }))
+	  .pipe(gulp.dest('out/'));
+  });
