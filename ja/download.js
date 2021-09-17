57ad8de268a6c4f0067c9b7724b9cafa4f05da1e
moritomomasa <s1870276@ems.u-toyama.ac.jp>
2021-09-18 04:41:32 +0900

    installation_url

--- ja/download.js
+++ ja/download.js
@@ -11,7 +11,7 @@ class Download extends HTMLElement {
             <img src="imgs/icon.png" alt="product icon"><br>
             <div>Ganimationのダウンロード</div>
           </div>
-          <button class="download-btn" onclick="window.open('https://drive.google.com/file/d/1_LwXFVwz85g6T6CV-kqXaWQYMxo8U23n/view?usp=sharing','_blank')">windows版</button>
+          <button class="download-btn" onclick="window.open('https://drive.google.com/drive/folders/1ZPVfz4f4PAbgHfV33CDD0TPmnyapCzX9','_blank')">windows版</button>
           <div class="spec-desc">
             <b>推奨スペック</b><br>
             <p>OS: Windows10 64-bit</p>
