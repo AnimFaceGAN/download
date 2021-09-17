e8f6de580f68bfbf0f9625e68574b08218b661e1
moritomomasa <s1870276@ems.u-toyama.ac.jp>
2021-09-17 16:44:58 +0900

    add_gitignore

--- ja/disp.js
+++
@@ -1,6 +0,0 @@
-
-// land 
-$(window).on('load resize',function(){
-    $('#main-container').css('width',  $(window).width());
-    $('#main-container').css('height',  $(window).height());
-});
--- ja/news.js
+++ ja/news.js
@@ -6,7 +6,7 @@ class News extends HTMLElement {
         <link rel="stylesheet" href="ja/style.css">
         <link rel="stylesheet" href="components/default.css">
 
-        <div id="news-container">
+        <div class="news-container">
           <ul class="news-list">  
             <li class="a-news" data-news="20210915">2021/9/15 : testtesttest</li>
             <li class="a-news" data-news="20210914">2021/9/14 : testtesttest</li>
