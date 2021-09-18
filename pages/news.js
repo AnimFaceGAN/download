e5f39662bcbba1f53ca53aac1552ea576f43754a
moritomomasa <s1870276@ems.u-toyama.ac.jp>
2021-09-18 20:54:14 +0900

    update_news2

--- pages/news.js
+++ pages/news.js
@@ -10,6 +10,9 @@ class News extends HTMLElement {
         <div class="news-container">
           <ul class="news-list">  
             <li class="a-news">
+              <a href="#" data-article-id="20210918-2" onclick="openModal(this);">2021/9/18 : 技育展に登壇します!!</a>
+            </li>
+            <li class="a-news">
               <a href="#" data-article-id="20210918-1" onclick="openModal(this);">2021/9/18 : 技育展の発表スライドURL</a>
             </li>
           </ul>
