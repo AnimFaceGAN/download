1fcd939877e671fba47ecde238b0522c397e4071
moritomomasa <s1870276@ems.u-toyama.ac.jp>
2021-09-19 03:58:04 +0900

    add_obs_desc

--- pages/download.js
+++ pages/download.js
@@ -14,12 +14,21 @@ class Download extends HTMLElement {
           </div>
           <button class="download-btn" onclick="window.open('https://drive.google.com/drive/folders/1ZPVfz4f4PAbgHfV33CDD0TPmnyapCzX9','_blank')">windows版</button>
           <div class="spec-desc">
+            <b>必要な外部ソフト</b><br>
+            <p>OBS Studio</p>
+            <p>↓ダウンロードURL</p>
+            <a href="#" onclick="window.open('https://obsproject.com/ja/download','_blank')">
+              https://obsproject.com/ja/download
+            </a>
+          </div>
+          <div class="spec-desc">
             <b>推奨スペック</b><br>
             <p>OS: Windows10 64-bit</p>
             <p>メモリー: 16GB RAM</p>
             <p>GPU: RTX2070super</p>
             <p>ストレージ: 5GB 利用可能</p>
           </div>
+          <div class="space"></div>
         </div>
 
         <!----- footer ----->
