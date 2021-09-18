4a00b752b622cf309fcad4ca6b3f92d3ba98ca41
moritomomasa <s1870276@ems.u-toyama.ac.jp>
2021-09-19 00:33:55 +0900

    support_safari

--- pages/land.js
+++ pages/land.js
@@ -11,15 +11,42 @@ class Land extends HTMLElement {
             <!-- float:left -->
             <div class="disp-left">
                 <div class="chars">
-                    <video src="imgs/chars/char1.webm" autoplay loop playsinline muted></video>
-                    <video src="imgs/chars/char2.webm" autoplay loop playsinline muted></video>
-                    <video src="imgs/chars/char3.webm" autoplay loop playsinline muted></video>
-                    <video src="imgs/chars/char4.webm" autoplay loop playsinline muted></video>
-                    <video src="imgs/chars/char5.webm" autoplay loop playsinline muted></video>
-                    <video src="imgs/chars/char6.webm" autoplay loop playsinline muted></video>
-                    <video src="imgs/chars/char7.webm" autoplay loop playsinline muted></video>
-                    <video src="imgs/chars/char8.webm" autoplay loop playsinline muted></video>
-                    <video src="imgs/chars/char9.webm" autoplay loop playsinline muted class="char9-video"></video>
+                    <video autoplay loop playsinline muted>
+                        <source src="imgs/chars/char1.webm" type="video/webm">
+                        <source src="imgs/chars/char1.mp4" type="video/mp4">
+                    </video>
+                    <video autoplay loop playsinline muted>
+                        <source src="imgs/chars/char2.webm" type="video/webm">
+                        <source src="imgs/chars/char2.mp4" type="video/mp4">
+                    </video>
+                    <video autoplay loop playsinline muted>
+                        <source src="imgs/chars/char3.webm" type="video/webm">
+                        <source src="imgs/chars/char3.mp4" type="video/mp4">
+                    </video>
+                    <video autoplay loop playsinline muted>
+                        <source src="imgs/chars/char4.webm" type="video/webm">
+                        <source src="imgs/chars/char4.mp4" type="video/mp4">
+                    </video>
+                    <video autoplay loop playsinline muted>
+                        <source src="imgs/chars/char5.webm" type="video/webm">
+                        <source src="imgs/chars/char5.mp4" type="video/mp4">
+                    </video>
+                    <video autoplay loop playsinline muted>
+                        <source src="imgs/chars/char6.webm" type="video/webm">
+                        <source src="imgs/chars/char6.mp4" type="video/mp4">
+                    </video>
+                    <video autoplay loop playsinline muted>
+                        <source src="imgs/chars/char7.webm" type="video/webm">
+                        <source src="imgs/chars/char7.mp4" type="video/mp4">
+                    </video>
+                    <video autoplay loop playsinline muted>
+                        <source src="imgs/chars/char8.webm" type="video/webm">
+                        <source src="imgs/chars/char8.mp4" type="video/mp4">
+                    </video>
+                    <video autoplay loop playsinline muted class="char9-video">
+                        <source src="imgs/chars/char9.webm" type="video/webm">
+                        <source src="imgs/chars/char9.mp4" type="video/mp4">
+                    </video>
                 </div>
             </div>
     
